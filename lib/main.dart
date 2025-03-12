164efe372a5ba4dd84a370b3ea8376dcadeb6d9b
yomnaabdelmegeed <yomna.developer@gmail>
2025-03-12 16:19:21 +0200

    Create project.

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const a7kely());
+}
+
+class a7kely extends StatelessWidget {
+  const a7kely({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
