3ee38678920399fa7a32884471e05f1140379268
yomnaabdelmegeed <yomna.developer@gmail>
2025-03-13 02:08:06 +0200

    Set app color.

---
+++ lib/color/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color offWhite = Color(0xfffbf5f2);
+  static const Color redOrange = Color(0xffDC3F00);
+  static const Color goldenYellow = Color(0xffEB9F4A);
+  static const Color mintGreen = Color(0xff77B29F);
+  static const Color purple = Color(0xffAB70DF);
+  static const Color blue = Color(0xff6884F6);
+}
